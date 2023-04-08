501eb77bb748cb6163db922fecde4c4ae06063fa
kelvinward1010 <kelvinward1010@gmail.com>
2023-04-08 09:02:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import ExcelJS from "exceljs";
+import { saveAs } from "file-saver";
+import Excel from "./components/Excel";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Excel />
 }
 
 export default App;
---
+++ src/components/Excel.js
@@ -0,0 +1,169 @@
+import React, { useState } from 'react'
+import { datafake } from '../data';
+import ExcelJS from 'exceljs';
+import { saveAs } from 'file-saver';
+
+function Excel() {
+
+    const data = datafake;
+    const [excelFilterEnabled, setExcelFilterEnabled] = useState(true)
+
+    const workSheetName = 'Work Sheet Test Name';
+    const workBookName = 'MyWorkBook';
+    const myInputId = 'myInput';
+
+    const handleExport = async () => {
+
+        //Change name file download
+        const myInput = document.getElementById(myInputId);
+        const fileName = myInput.value || workBookName;
+
+        // const workbook = new ExcelJS.Workbook();
+        const htmlTable = document.getElementById('myTable');
+ 
+        // Create a new workbook and worksheet using ExcelJS
+        const workbook = new ExcelJS.Workbook();
+        //const worksheet = workbook.addWorksheet(workSheetName);
+        const worksheet =  workbook.addWorksheet(workSheetName, {
+            pageSetup:{paperSize: 9, orientation:'landscape'}
+        });
+        worksheet.mergeCells('A2', 'I2');
+
+        const customCell = worksheet.getCell("A2");
+        customCell.font = {
+            name: "Comic Sans MS",
+            family: 4,
+            size: 5,
+            underline: true,
+            bold: true
+        };
+ 
+        // Add the column names to the worksheet
+        const headerRow = worksheet.addRow([]);
+        worksheet.getRow(4).font = { bold: true };
+        const headerCells = htmlTable.getElementsByTagName('th');
+        for (let i = 0; i < headerCells.length; i++) {
+            headerRow.getCell(i + 1).value = headerCells[i].innerText;
+        }
+ 
+        // Add the HTML table data to the worksheet
+        const rows = htmlTable.getElementsByTagName('tr');
+        for (let i = 0; i < rows.length; i++) {
+            const cells = rows[i].getElementsByTagName('td');
+            const rowData = [];
+
+            for (let j = 0; j < cells.length; j++) {
+                rowData.push(cells[j].innerText);
+            }
+            worksheet.addRow(rowData);
+        }
+
+        worksheet.columns.forEach(column => {
+            column.width = headerCells.length + 10
+            column.alignment = { horizontal: 'right' };
+        });
+
+        worksheet.autoFilter = {
+            from: {
+              row: 4,
+              column: 1
+            },
+            to: {
+              row: 4,
+              column: 10
+            }
+        };
+
+        worksheet.eachRow({ includeEmpty: false }, row => {
+            // store each cell to currentCell
+            const currentCell = row._cells;
+    
+            // loop through currentCell to apply border only for the non-empty cell of excel
+            currentCell.forEach(singleCell => {
+              // store the cell address i.e. A1, A2, A3, B1, B2, B3, ...
+              const cellAddress = singleCell._address;
+    
+              // apply border
+              worksheet.getCell(cellAddress).border = {
+                top: { style: 'thin' },
+                left: { style: 'thin' },
+                bottom: { style: 'thin' },
+                right: { style: 'thin' }
+              };
+            });
+        });
+
+        worksheet.eachRow({ includeEmpty: false }, row => {
+            // store each cell to currentCell
+            const currentCell = row._cells;
+    
+            // loop through currentCell to apply border only for the non-empty cell of excel
+            currentCell.forEach(singleCell => {
+              // store the cell address i.e. A1, A2, A3, B1, B2, B3, ...
+              const cellAddress = singleCell._address;
+    
+              // apply border
+              worksheet.getCell(cellAddress).border = {
+                top: { style: 'thin' },
+                left: { style: 'thin' },
+                bottom: { style: 'thin' },
+                right: { style: 'thin' }
+              };
+            });
+        });
+ 
+        // Generate a blob object from the workbook and download it as an attachment
+        const buf = await workbook.xlsx.writeBuffer();
+        workbook.xlsx.writeBuffer().then(function (buffer) {
+            const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        });
+        saveAs(new Blob([buf]), `${fileName}.xlsx`);
+    }
+
+    return (
+        <div>
+            <div>
+                Export to excel from table
+                <br />
+                <br />
+                Export to : <input id={myInputId} defaultValue={workBookName} /> .xlsx
+            </div>
+            <table id="myTable">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>FirstName</th>
+                        <th>LastName</th>
+                        <th>Prefix</th>
+                        <th>Position</th>
+                        <th>BirthDate</th>
+                        <th>HireDate</th>
+                        <th>State</th>
+                        <th>City</th>
+                        <th>SaleAmount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item,idx) => (
+                        <tr key={idx}>
+                            <td>{item.ID}</td>
+                            <td>{item.FirstName}</td>
+                            <td>{item.LastName}</td>
+                            <td>{item.Prefix}</td>
+                            <td>{item.Position}</td>
+                            <td>{item.BirthDate}</td>
+                            <td>{item.HireDate}</td>
+                            <td>{item.Notes}</td>
+                            <td>{item.State}</td>
+                            <td>{item.City}</td>
+                            <td>{item.SaleAmount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <button onClick={handleExport}>Export to Excel</button>
+        </div>
+    )
+}
+
+export default Excel
